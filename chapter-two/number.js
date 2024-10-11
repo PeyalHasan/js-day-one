f260857de3353f9bbc221c4dc93cac944b4a4960
unknown <hpeyal7@gmail.com>
2024-10-11 18:14:39 +0600

    complite day one

---
+++ chapter-two/number.js
@@ -0,0 +1,21 @@
+var n =343;
+var f = 3.44
+var nl = Number('45')
+console.log(nl)
+console.log(Number.parseFloat(nl))
+console.log(Number.parseInt(f))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_VALUE)
+
+var a = 10;
+var b = a / 0;
+console.log(b)
+
+var c = 'Peyal';
+var d = c / 3
+console.log(d)
+console.log(isNaN(c))
+console.log(isNaN(a))
+console.log(isNaN(NaN))
---
+++ chapter-two/string.js
@@ -0,0 +1,9 @@
+var str1 = 'String'
+var str2 =  "String"
+var str3 = `String`
+
+var str4 = String('Peyal')
+var str5 = String(424)
+var str6 = String(13.34)
+
+console.log(str1, str2, str3, str4, str5, str6)
